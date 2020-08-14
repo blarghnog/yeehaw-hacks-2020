2a1d61f06d839caa5748a81ba1034d7439366743
blarghnog <jtn2km@virginia.edu>
2020-08-14 18:32:44 -0400

    test implementation of map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import GoogleMap from 'google-map-react';
 
 function App() {
-    return(<p>pop</p>)
+    const location = {
+        address: "1600 Amphitheatre Parkway, Mountain View, california.",
+        lat: 37.42216,
+        lng: -122.08427,
+    };
+
+    return(
+        <div style={{ height: "100vh", width: "100%"}}>
+            <GoogleMap
+                center = {location}
+                zoom = {11}
+            >
+                <Test
+                    lat = {location.lat}
+                    lng = {location.lng}
+                ></Test>
+            </GoogleMap>
+        </div>
+    );
 }
 export default App;
+
+function Test() {
+    return(
+        <p>poop</p>
+    )
+}
